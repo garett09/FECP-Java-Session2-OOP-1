6c02049ceed198e88bee1abf7fa0258f2117785d
Adrian Garett Sian <adriansian@gmail.com>
2025-07-03 15:22:39 +0800

    oop: add methods for banking system
    incomplete

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -34,10 +34,28 @@ public class BankAccount {
          this.availableBalance=availableBalance;
     }
 
-    
-
-
-
-
-
+    public void deposit(int amount){
+        if(amount > 0){
+            this.availableBalance += amount;
+            System.out.println("Deposit successful. New balance: " + this.availableBalance);
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+    public void withdraw(int amount){
+        if(amount > 0 && amount <= this.availableBalance){
+            this.availableBalance -= amount;
+            System.out.println("Withdrawal successful. New balance: " + this.availableBalance);
+        } else if (amount > this.availableBalance) {
+            System.out.println("Insufficient funds for withdrawal.");
+        } else {
+            System.out.println("Withdrawal amount must be positive.");
+        }
+    }
+    public void displayAccountInfo(){
+        System.out.println("Account Number: " + this.accountNumber);
+        System.out.println("Account Holder Name: " + this.acctHolderName);
+        System.out.println("Available Balance: " + this.availableBalance);
+    }
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<String> bankAccount = new ArrayList<>();
+        bankAccount.add("John Doe - $1000");
+        System.out.println("Bank Account Details:" + bankAccount);
 
     }
 }
