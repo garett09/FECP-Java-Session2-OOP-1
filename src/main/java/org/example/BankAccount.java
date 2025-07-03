f4db91f3ba811cb3d86170affeb5be3c97d1e19f
Adrian Garett Sian <adriansian@gmail.com>
2025-07-03 15:11:44 +0800

    oop: add class for bankAccount

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,4 +1,43 @@
 package org.example;
 
 public class BankAccount {
+    private int accountNumber;
+    private String acctHolderName;
+    private int availableBalance;
+
+    public BankAccount(int accountNumber, String acctHolderName, int availableBalance){
+        this.accountNumber = accountNumber;
+        this.acctHolderName = acctHolderName;
+        this.availableBalance = availableBalance;
+    }
+
+    public int getAccountNumber(){
+        return this.accountNumber;
+    }
+    public void setAccountNumber(){
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAcctHolderName(){
+        return this.acctHolderName;
+    }
+
+    public void setAcctHolderName(){
+        this.acctHolderName = acctHolderName;
+    }
+
+    public int getAvailableBalance(){
+        return this.availableBalance;
+    }
+
+    public void setAvailableBalance(){
+         this.availableBalance=availableBalance;
+    }
+
+    
+
+
+
+
+
 }
